6048d8e79b0dd8ee935e72f7daed623af793e58c
Odelia Minentle Ntontela <163156732+MinnieNtontela@users.noreply.github.com>
2026-08-18 23:16:26 +0200

    un-commmented my examples

--- examples/Odelia/pyautogui/mousecontrol.py
+++ examples/Odelia/pyautogui/mousecontrol.py
@@ -1,10 +1,10 @@
-'''import pyautogui
+import pyautogui
 import time
 
 # Get current position
-x, y = pyautogui. position()
+x, y = pyautogui.position()
 print(f"Mouse at: {x}, {y}")
 
 # Move mouse smoothly
 pyautogui.moveTo(100, 100, duration=1)
-pyautogui.click()'''
+pyautogui.click()
